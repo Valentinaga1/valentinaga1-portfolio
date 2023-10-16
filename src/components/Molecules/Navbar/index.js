40d126d39704768b5f256356965417e8c3b98362
valentinaga1 <valentinagoa2@gmail.com>
2023-10-15 19:40:55 -0500

    Removing some comments

--- src/components/Molecules/Navbar/index.js
+++ src/components/Molecules/Navbar/index.js
@@ -3,21 +3,22 @@ import styles from "./styles.module.css";
 import { links } from '@/data';
 
 const Navbar = () => {
-
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            targetElement.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
+  if (typeof window !== 'undefined') {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+  
+          const targetId = this.getAttribute('href').substring(1);
+          const targetElement = document.getElementById(targetId);
+  
+          if (targetElement) {
+              targetElement.scrollIntoView({
+                  behavior: 'smooth'
+              });
+          }
+      });
     });
-});
+  }
 
   return (
     <div className={styles["navbar_main"]} id="home">
